c779800e0696d849f84587436b7db14930cba1e9
Eswar Kumar <keswar431@gmail.com>
2026-03-24 15:00:14 +0530

    updated the authcontroller and update the jwt,global exception handling

---
+++ AuthAndSecurity/HotelBookingSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using HotelBookingSystem.API.DTOs.Auth;
+using HotelBookingSystem.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+            return Ok(result);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+            return Ok(result);
+        }
+    }
+}
---
+++ AuthAndSecurity/HotelBookingSystem.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingSystem.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingSystem.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    
+
+      
+    }
+}
